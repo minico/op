d17385824451c089c2171f86b3e0f257c47cf1d5
minico <xj.zhang@msn.com>
2022-06-05 21:09:04 +0800

    disable patch by default

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,34 +20,45 @@ cp /usr/bin/upx ./staging_dir/host/bin/
 #sed -i "/dnsmasq \\\/d" include/target.mk
 
 # apply patches
+
 ### patch for disable resolve ipv6 address
-patch -Np1 <$GITHUB_WORKSPACE/patch/dnsmasq/dnsmasq-add-filter-aaaa-option.patch
-patch -Np1 <$GITHUB_WORKSPACE/patch/dnsmasq/luci-add-filter-aaaa-option.patch
-cp -f $GITHUB_WORKSPACE/patch/dnsmasq/900-add-filter-aaaa-option.patch ./package/network/services/dnsmasq/patches/900-add-filter-aaaa-option.patch
+apply_patch_ipv6_dns() {
+    patch -Np1 <$GITHUB_WORKSPACE/patch/dnsmasq/dnsmasq-add-filter-aaaa-option.patch
+    patch -Np1 <$GITHUB_WORKSPACE/patch/dnsmasq/luci-add-filter-aaaa-option.patch
+    cp -f $GITHUB_WORKSPACE/patch/dnsmasq/900-add-filter-aaaa-option.patch ./package/network/services/dnsmasq/patches/900-add-filter-aaaa-option.patch
+}
 
 ### Fullcone-NAT 部分 ###
-# Patch Kernel 以解决 FullCone 冲突
-#pushd target/linux/generic/hack-5.4
-#wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
-#popd
-cp -f $GITHUB_WORKSPACE/patch/firewall/952-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.4
+apply_patch_full_cone() {
+    # Patch Kernel 以解决 FullCone 冲突
+    #pushd target/linux/generic/hack-5.4
+    #wget https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+    #popd
+    cp -f $GITHUB_WORKSPACE/patch/firewall/952-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.4
 
-# Patch FireWall 以增添 FullCone 功能
-mkdir -p package/network/config/firewall/patches
-#wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
-cp $GITHUB_WORKSPACE/patch/firewall/fullconenat.patch ./package/network/config/firewall/patches/
-#wget -qO- https://github.com/msylgj/R2S-R4S-OpenWrt/raw/21.02/PATCHES/001-fix-firewall-flock.patch | patch -p1
-patch -Np1 <$GITHUB_WORKSPACE/patch/firewall/001-fix-firewall-flock.patch
+    # Patch FireWall 以增添 FullCone 功能
+    mkdir -p package/network/config/firewall/patches
+    #wget -P package/network/config/firewall/patches/ https://github.com/immortalwrt/immortalwrt/raw/master/package/network/config/firewall/patches/fullconenat.patch
+    cp $GITHUB_WORKSPACE/patch/firewall/fullconenat.patch ./package/network/config/firewall/patches/
+    #wget -qO- https://github.com/msylgj/R2S-R4S-OpenWrt/raw/21.02/PATCHES/001-fix-firewall-flock.patch | patch -p1
+    patch -Np1 <$GITHUB_WORKSPACE/patch/firewall/001-fix-firewall-flock.patch
+    
+    # Patch LuCI 以增添 FullCone 开关
+    patch -Np1 <$GITHUB_WORKSPACE/patch/firewall/luci-app-firewall_add_fullcone.patch
+    # FullCone 相关组件
+    #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/openwrt-fullconenat package/lean/openwrt-fullconenat
+    #mkdir -p package/lean/openwrt-fullconenat
+    #cp -rf $GITHUB_WORKSPACE/patch/fullconenat/* ./package/lean/openwrt-fullconenat/ 
+    #pushd package/lean/openwrt-fullconenat
+    #patch -Np2 <$GITHUB_WORKSPACE/patch/firewall/fullcone6.patch
+    #popd
+}
 
-# Patch LuCI 以增添 FullCone 开关
-patch -Np1 <$GITHUB_WORKSPACE/patch/firewall/luci-app-firewall_add_fullcone.patch
-# FullCone 相关组件
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/openwrt-fullconenat package/lean/openwrt-fullconenat
-#mkdir -p package/lean/openwrt-fullconenat
-#cp -rf $GITHUB_WORKSPACE/patch/fullconenat/* ./package/lean/openwrt-fullconenat/ 
-#pushd package/lean/openwrt-fullconenat
-#patch -Np2 <$GITHUB_WORKSPACE/patch/firewall/fullcone6.patch
-#popd
+if [ $APPLY_PATCH = "true" ];then
+    apply_patch_ipv6_dns
+    apply_patch_full_cone
+fi
 
 DATE=`date "+%Y-%m-%d %H:%M"`
 sed -i "s/.*github.*/\t\tCompiled by IVAN.ZHANG $DATE/" ./feeds/luci/themes/luci-theme-bootstrap/luasrc/view/themes/bootstrap/footer.htm
+sed -i "s/.*ZHANG.*/\t\tCompiled by IVAN.ZHANG $DATE/" ./feeds/luci/themes/luci-theme-bootstrap/luasrc/view/themes/bootstrap/footer.htm
